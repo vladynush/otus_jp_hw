02ebd2b79144e80b803e912389a8fb7bc82b009f
vladynush <vladryzov@gmail.com>
2023-12-03 13:20:33 +0300

    HW-13 1. Реализация (на основе hw-12) 2. Добавлена конфигурация hibernate

---
+++ hw-13/src/main/java/ru/otus/core/sessionmanager/DataBaseOperationException.java
@@ -0,0 +1,8 @@
+package ru.otus.core.sessionmanager;
+
+public class DataBaseOperationException extends RuntimeException {
+
+    public DataBaseOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
