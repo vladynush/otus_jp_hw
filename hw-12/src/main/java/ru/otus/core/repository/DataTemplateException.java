78daa7d7fac52729e01930a32490611224862be2
vladynush <vladryzov@gmail.com>
2023-12-03 13:08:45 +0300

    HW-12 1. Добавление класса Менеджера 1. Реализация сервисов

---
+++ hw-12/src/main/java/ru/otus/core/repository/DataTemplateException.java
@@ -0,0 +1,8 @@
+package ru.otus.core.repository;
+
+public class DataTemplateException extends RuntimeException {
+
+    public DataTemplateException(Exception ex) {
+        super(ex);
+    }
+}
---
+++ hw-12/src/main/java/ru/otus/jdbc/mapper/EntitySQLMetaData.java
@@ -0,0 +1,16 @@
+package ru.otus.jdbc.mapper;
+
+/**
+ * Создает SQL - запросы
+ */
+public interface EntitySQLMetaData {
+    String getSelectAllSql();
+
+    String getSelectByIdSql();
+
+    String getInsertSql();
+
+    String getUpdateSql();
+
+    EntityClassMetaData getEntityClassMetaData();
+}
